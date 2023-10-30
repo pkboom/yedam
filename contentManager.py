1610c3bbff57c5b357da7549bb8e8ea3aa718f49
Keunbae Park <pkboom@users.noreply.github.com>
2023-10-29 21:23:06 -0400

    wip

---
+++ contentManager.py
@@ -0,0 +1,12 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def hello_context_manager():
+    print("Entering the context...")
+    yield "Hello, World!"
+    print("Leaving the context...")
+
+
+with hello_context_manager() as hello:
+    print(hello)
